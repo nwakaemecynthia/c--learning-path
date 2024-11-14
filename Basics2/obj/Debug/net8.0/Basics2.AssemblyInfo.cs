c9bb69613ed165bd0ffda7ca63a891a69111fd49
nwakaemecynthia <nwakaemecynthia@gmail.com>
2024-11-14 14:24:07 +0100

    non- primitive datatypes

--- Basics2/obj/Debug/net8.0/Basics2.AssemblyInfo.cs
+++ Basics2/obj/Debug/net8.0/Basics2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+806cfcaa22062f327fe1d9d0253ad400ef927c50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
