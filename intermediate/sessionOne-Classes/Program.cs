0550d3adfa0110a680132ce8c3ec1dac4b2d7519
nwakaemecynthia <nwakaemecynthia@gmail.com>
2024-11-21 23:29:37 +0100

    Initialised Intermediate learning path

---
+++ intermediate/sessionOne-Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SessionOne
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*********************************************************************************
+                 ************** WHAT IS A CLASSE
+                    This is a building block of an application.
+                 ************** ANATOMY OF A CLASS
+                    1) Data: (repersented by feilds or properties of a class)
+                    1) Behaviour: (repersented by methods/functions of a class)
+                 ************** WHAT IS AN OBJECT
+                    An object is an instance of a class that resides in memory.
+                 ************** STATIC MEMBERS  
+
+            *********************************************************************************/
+            //Declaring a class
+            public class Person
+            {
+                
+            }
+        }
+    }
+}
